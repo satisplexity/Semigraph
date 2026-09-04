a2174efa78d5f3aaf08bf6e2505bccf81301ef86
satisplexity <satisplexity@gmail.com>
2026-09-04 02:13:12 +0200

    feat(presentation): add Semigraph.Presentation project

---
+++ Semigraph.Presentation/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Semigraph.Presentation.Views.Windows;
+using System.Windows;
+
+namespace Semigraph.Presentation;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs arguments)
+    {
+        base.OnStartup(arguments);
+
+        ShowWindow();
+    }
+
+    private void ShowWindow()
+    {
+        MainWindow window = new();
+
+        window.Show();
+    }
+}
---
+++ Semigraph.Presentation/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Semigraph.Presentation.Views.Windows;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
